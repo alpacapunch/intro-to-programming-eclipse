264a09306bd74ddfd6ea5d6b6a173e06e8595b5d
alpacapunch <73743661+alpacapunch@users.noreply.github.com>
2021-06-18 17:10:35 -0400

    lesson 4-3

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// -- lesson 4-2
 //inserting copyright text in footer
 const today = new Date();
 const thisYear = today.getFullYear();
@@ -23,3 +24,49 @@ for (let i = 0; i < skills.length; i++) {
   //append 'skill' to 'skillsList'
   skillsList.appendChild(skill);
 }
+
+// -- lesson 4-3
+//selection
+const messageForm = document.querySelector('[name=leave_message]')
+//event listener
+messageForm.addEventListener('submit', (Event) => {
+  //preventing default submit event
+  Event.preventDefault()
+  //form field variables retrieving input
+  const name = Event.target.name.value
+  console.log('name')
+  const email = Event.target.email.value
+  console.log('email')
+  const message = Event.target.message.value
+  console.log('message')
+  //selection
+  const messageSection = document.querySelector('#messages')
+  //selection
+  const messageList = messageSection.querySelector('ul')
+  //create
+  const newMessage = document.createElement('li')
+  //set 
+  newMessage.innerHTML = `<a href = "mailto:${email}">${name}</a> wrote: <span>${message}</span> `
+  //remove button
+  const removeButton = document.createElement('button')
+  //setting inner text
+  removeButton.innerText = 'remove'
+  //setting type attribute
+  removeButton.type = 'button'
+  //click event
+  removeButton.addEventListener('click', (Event) => {
+    //finding parent element
+    const entry = Event.target.parentNode
+    entry.remove()
+  })
+  //hide #messages when empty
+  if (messageList.children.length < 1) {
+    messageSection.style.display = 'none'
+  }
+  //append removeButton
+  newMessage.appendChild(removeButton)
+  //append newMessage
+  messageList.appendChild(newMessage)
+  //reset form
+  Event.target.reset()
+})
